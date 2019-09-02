b0b48766d93bc5112eac157a02263583bcdb0554
artem.horobei <artem.horobei@yalantis.com>
2019-09-02 13:35:15 +0300

    Add draft with session token

--- src/pages/Users/hooks.ts
+++ src/pages/Users/hooks.ts
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const CLIENT_ID = "3e014187322f4055ddab";
+const CLIENT_SECRET = "f80e8e2a82ea5d8a0b41c0d63c09c76069f4b24e";
+const REDIRECT_URI = "https://398b43ff.ngrok.io/users";
+
 export type TUser = {
     avatar_url: string,
     login: string,
@@ -9,10 +13,17 @@ export type TUser = {
 
 export interface IUseUsers {
     users: TUser[],
+    oAuthCode?: string,
+    sessionData?: {
+        token?: string,
+        type?: string,
+    }
 }
 
 function useUsers(initialState: []): IUseUsers {
     const [users, setUsers] = useState(initialState);
+    const [oAuthCode, setOauthCode] = useState('');
+    const [sessionData, setSessionData] = useState({ token: '', type: '' });
 
     useEffect(() => {
         axios
@@ -21,8 +32,41 @@ function useUsers(initialState: []): IUseUsers {
             .catch(e => console.log(e));
     }, []);
 
+    useEffect(() => {
+        const oAuthCode = window.location.href.split('?code=')[1];
+        if (oAuthCode) {
+            setOauthCode(oAuthCode);
+            axios
+                .post(
+                    `https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token`,
+                    {
+                        client_id: CLIENT_ID,
+                        client_secret: CLIENT_SECRET,
+                        code: oAuthCode,
+                        redirect_uri: REDIRECT_URI,
+                    },
+                    {
+                        headers: {
+                            Accept: 'application/json',
+                        },
+                    },
+                )
+                .then(({
+                    data: {
+                        access_token: token,
+                        token_type: type,
+                    },
+                }) => setSessionData({
+                    token,
+                    type,
+                }));
+        }
+    }, []);
+
     return {
         users,
+        oAuthCode,
+        sessionData,
     };
 }
 
--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -3,10 +3,25 @@ import * as React from 'react';
 import useUsers, { IUseUsers } from './hooks';
 import UsersListing from '../../components/UsersListing';
 
-function Users(): React.FunctionComponentElement<{}> {
-    const { users }: IUseUsers = useUsers([]);
+const CLIENT_ID = "3e014187322f4055ddab";
+const REDIRECT_URI = "https://398b43ff.ngrok.io/users";
 
-    return <UsersListing users={users} />;
+function Users(): React.FunctionComponentElement<{}> {
+    const { users, sessionData }: IUseUsers = useUsers([]);
+
+    console.log(sessionData);
+
+    return (
+        <div>
+            <a
+                style={{ display: 'block', margin: '3rem 0' }}
+                href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}
+            >
+                Login
+            </a>
+            <UsersListing users={users} />
+        </div>
+    );
 }
 
 export default Users;
